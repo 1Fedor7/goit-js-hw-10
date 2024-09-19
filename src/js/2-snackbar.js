58cdeed9d3880f898f8e257b0f648a5038fa9b47
Fedor <fiafedor7@gmail.com>
2024-09-19 21:51:56 +0200

    added code to the second task. installed npm install izitoast.

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,37 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+document.querySelector('.form').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const delay = Number(event.target.elements.delay.value);
+  const state = event.target.elements.state.value;
+
+  createPromise(delay, state)
+    .then((result) => {
+      iziToast.success({
+        title: '✅ Success',
+        message: `Fulfilled promise in ${result}ms`,
+        position: 'topRight'
+      });
+    })
+    .catch((error) => {
+      iziToast.error({
+        title: '❌ Error',
+        message: `Rejected promise in ${error}ms`,
+        position: 'topRight'
+      });
+    });
+});
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
